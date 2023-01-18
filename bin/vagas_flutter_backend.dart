750b92db869891483ee6b090e30609641b0b2fee
LeandriSimo <leo.bt.12@hotmail.com>
2023-01-18 15:29:01 -0300

    Subindo o projeto

---
+++ bin/vagas_flutter_backend.dart
@@ -0,0 +1,5 @@
+import 'package:vagas_flutter_backend/vagas_flutter_backend.dart' as vagas_flutter_backend;
+
+void main(List<String> arguments) {
+  print('Hello world: ${vagas_flutter_backend.calculate()}!');
+}
---
+++ test/vagas_flutter_backend_test.dart
@@ -0,0 +1,8 @@
+import 'package:vagas_flutter_backend/vagas_flutter_backend.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
