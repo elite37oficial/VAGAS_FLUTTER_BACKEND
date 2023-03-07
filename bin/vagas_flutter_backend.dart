280f195a9403d04e170d480fa847105f5fa1b898
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-03-06 21:06:31 -0300

    [feat]: criação das primeiras rotas e adição do middleware logRequest

--- bin/vagas_flutter_backend.dart
+++ bin/vagas_flutter_backend.dart
@@ -1,3 +1,5 @@
+import 'package:shelf_router/shelf_router.dart';
+
 import 'custom_server.dart';
 import 'package:shelf/shelf.dart';
 import 'package:dotenv/dotenv.dart';
@@ -5,8 +7,20 @@ import 'package:dotenv/dotenv.dart';
 void main() async {
   var env = DotEnv(includePlatformEnvironment: true)..load();
 
+  final router = Router();
+  router.post('/login', (Request request) async {
+    final body = await request.readAsString();
+    return Response.ok(body, headers: {'content-type': 'application/json'});
+  });
+
+  router.get('/jobs', (Request request) async {
+    return Response.ok('Lista de vagas');
+  });
+
+  final pipeline = Pipeline().addMiddleware(logRequests()).addHandler(router);
+
   await CustomServer().initilize(
-    handler: (request) => Response.ok('hello world'),
+    handler: pipeline,
     address: env['address'] ?? 'localhost',
     port: int.parse(env['port'].toString()),
   );
