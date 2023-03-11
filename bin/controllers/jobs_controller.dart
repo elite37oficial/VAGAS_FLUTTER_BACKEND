19983d3e09ea2ac7f6e34f5cbb3c6e6f0295c8ef
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-03-11 07:58:35 -0300

    [feat]: cria rota post para jobs e rota adiciona serialização para retorno de dados

--- bin/controllers/jobs_controller.dart
+++ bin/controllers/jobs_controller.dart
@@ -1,12 +1,28 @@
+import 'dart:convert';
+
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
+import '../models/job_model.dart';
+import '../services/generic_service.dart';
+
 class JobsController {
+  final GenericService<JobModel> _service;
+
+  JobsController(this._service);
   Handler get handler {
     var router = Router();
 
     router.get('/jobs', (Request request) async {
-      return Response.ok("Lista de vagas");
+      List<JobModel> jobsList = await _service.findAll();
+      List<Map> jobsMap = jobsList.map((job) => job.toJson()).toList();
+      return Response.ok(jsonEncode(jobsMap));
+    });
+
+    router.post('/jobs', (Request request) async {
+      var body = await request.readAsString();
+      _service.save(JobModel.fromJson(jsonDecode(body)));
+      return Response(201);
     });
 
     return router;
