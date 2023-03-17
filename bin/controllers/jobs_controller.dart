4572c68637ffa98cd740ae01a8791d2204f07552
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-03-17 20:15:08 -0300

    [refact]:  injeta implementação de DAO na camada Service e remove do controller

--- bin/controllers/jobs_controller.dart
+++ bin/controllers/jobs_controller.dart
@@ -3,20 +3,18 @@ import 'dart:convert';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-import '../dao/dao.dart';
 import '../models/job_model.dart';
 import '../services/generic_service.dart';
 
 class JobsController {
   final GenericService<JobModel> _service;
-  final DAO _dao;
 
-  JobsController(this._service, this._dao);
+  JobsController(this._service);
   Handler get handler {
     var router = Router();
 
     router.get('/jobs', (Request request) async {
-      var result = await _dao.findAll();
+      var result = await _service.findAll();
       return Response.ok(jsonEncode(result));
     });
 
@@ -25,20 +23,20 @@ class JobsController {
       if (id == null) {
         return Response(400);
       }
-      var result = await _dao.delete(id);
+      var result = await _service.delete(id);
       return result ? Response(200) : Response.internalServerError();
     });
 
     router.put('/jobs', (Request request) async {
       final String body = await request.readAsString();
-      var result = await _dao.update(JobModel.fromJson(jsonDecode(body)));
+      var result = await _service.save(JobModel.fromJson(jsonDecode(body)));
       return result ? Response(500) : Response(500);
     });
 
     router.post('/jobs', (Request request) async {
       var body = await request.readAsString();
       JobModel jobmodel = JobModel.fromJson(jsonDecode(body));
-      var result = await _dao.create(jobmodel);
+      var result = await _service.save(jobmodel);
       return result ? Response(201) : Response(404);
     });
 
