b3d6a0ce775bcb08434201c25b100c9a02ab3630
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-03-17 19:39:05 -0300

    [feat]: adiciona dependencia do Uuid

--- bin/dao/jobs_dao.dart
+++ bin/dao/jobs_dao.dart
@@ -1,18 +1,21 @@
+import 'package:uuid/uuid.dart';
+
 import '../database/db_configuration.dart';
 import '../models/job_model.dart';
 import 'dao.dart';
 
 class JobDAO implements DAO<JobModel> {
   final DBConfiguration _dbConfiguration;
+  final uuid = Uuid();
 
   JobDAO(this._dbConfiguration);
 
   @override
   Future<bool> create(JobModel value) async {
     var result = await _dbConfiguration.execQuery(
-        'INSERT INTO jobs (id, companyId, title, description, salary, location,seniority, regime, link, whatsapp, email, createdBy, createdDate, updatedBy, updatedDate) values (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?); ',
+        'INSERT INTO jobs (id, companyId, title, description, salary, location,seniority, regime, link, whatsapp, email, createdBy, createdDate) values (?,?,?,?,?,?,?,?,?,?,?,?,?); ',
         [
-          'UUID()',
+          uuid.v1(),
           value.companyId,
           value.title,
           value.description,
@@ -24,9 +27,7 @@ class JobDAO implements DAO<JobModel> {
           value.whatsappNumber,
           value.email,
           value.createdBy,
-          value.createdDate,
-          value.changedBy,
-          value.changedDate
+          value.createdDate
         ]);
     return result.affectedRows > 0;
   }
@@ -56,6 +57,7 @@ class JobDAO implements DAO<JobModel> {
 
   @override
   Future<bool> update(JobModel value) async {
+    final DateTime today = DateTime.now().toUtc();
     var result = await _dbConfiguration.execQuery(
         'UPDATE jobs set companyId = ?, title = ?, description = ?, salary = ?, local = ?,seniority = ?, regime = ?, link = ?, whatsappNumber = ?, email = ?, createdBy = ?, createdDate = ?, changedBy = ?, changedDate = ?, where id = ?; ',
         [
@@ -71,7 +73,7 @@ class JobDAO implements DAO<JobModel> {
           value.createdBy,
           value.createdDate,
           value.changedBy,
-          'NOW()'
+          today
         ]);
 
     return result.affectedRows > 0;
