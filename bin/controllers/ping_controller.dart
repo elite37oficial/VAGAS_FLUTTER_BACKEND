32891130ab682da410eca2b582cf53df4382e1c2
Valdir Giorgi Notebook <valdirgiorgi@gmail.com>
2023-03-17 23:01:07 -0300

    testado atualização automatica

--- bin/controllers/ping_controller.dart
+++ bin/controllers/ping_controller.dart
@@ -7,7 +7,7 @@ class PingController {
 
     router.get('/ping', (Request request) async {
       final body = await request.readAsString();
-      return Response.ok('OK 2');
+      return Response.ok('OK 3');
     });
 
     return router;
