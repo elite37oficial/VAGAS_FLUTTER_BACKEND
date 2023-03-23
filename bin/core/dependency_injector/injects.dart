f1caedf04fb251b73d8797900c516ddf44b72e7d
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-03-23 12:23:25 -0300

    [refact]: registra servico de segurança nas in jeções de dependencia

--- bin/core/dependency_injector/injects.dart
+++ bin/core/dependency_injector/injects.dart
@@ -1,14 +1,22 @@
+import 'package:dotenv/dotenv.dart';
+
 import '../../controllers/jobs_controller.dart';
 import '../../controllers/login_controller.dart';
 import '../../controllers/ping_controller.dart';
 import '../../dao/jobs_dao.dart';
 import '../../database/mysql_db_configuration.dart';
 import '../../services/jobs_service.dart';
+import '../security/security_service.dart';
+import '../security/security_service_imp.dart';
 import 'dependency_injector.dart';
 
 class Injects {
   static DependencyInjector initialize() {
     var di = DependencyInjector();
+
+    di.register<DotEnv>(() => DotEnv(includePlatformEnvironment: true)..load());
+
+    di.register<SecurityService>(() => SecurityServiceImp());
 
     di.register<LoginController>(() => LoginController());
     di.register<PingController>(() => PingController());
