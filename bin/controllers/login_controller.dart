19910b04198674dc30e4c1123b8317f89be0daf7
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-03-26 09:40:35 -0300

    [refact]: implementa feature de login e resolve injeções de dependencia

--- bin/controllers/login_controller.dart
+++ bin/controllers/login_controller.dart
@@ -1,31 +1,37 @@
 import 'dart:convert';
-
-import 'package:dart_jsonwebtoken/dart_jsonwebtoken.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
-import '../core/dependency_injector/dependency_injector.dart';
 import '../core/security/security_service.dart';
+import '../models/user_model.dart';
+import '../services/auth_service.dart';
+import '../to/auth_to.dart';
 import 'controller.dart';
 
 class LoginController extends Controller {
+  final AuthService _authService;
+  final SecurityService _securityServiceImp;
+  LoginController(this._authService, this._securityServiceImp);
+
   @override
   Handler getHandler({List<Middleware>? middlewares, bool isSecurity = false}) {
     var router = Router();
 
-    DependencyInjector di = DependencyInjector();
-    SecurityService securityServiceImp = di.get<SecurityService>();
-
     router.post('/login', (Request request) async {
-      // final body = await request.readAsString();
+      final body = await request.readAsString();
+      final AuthTO authTO = AuthTO.fromRequest(body);
+
       //lógica para validar email e senha com o banco
-      final String token = await securityServiceImp.generateJWT('userID');
-      final JWT? validateToken = await securityServiceImp.validateJWT(token);
-      return Response.ok(jsonEncode({
-        'token': token,
-        'isValid': validateToken != null,
-        'roles': validateToken?.payload['roles']
-      }));
+      final UserModel? userModel = await _authService.authenticate(authTO);
+      if (userModel != null) {
+        final String userID = userModel.id!;
+        final String profileID = userModel.profileId!;
+        final String token =
+            await _securityServiceImp.generateJWT(userID, profileID);
+        return Response.ok(jsonEncode({'token': token}));
+      } else {
+        return Response(401);
+      }
     });
 
     return createHandler(
