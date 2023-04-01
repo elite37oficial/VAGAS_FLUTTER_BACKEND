4ace14408b4c65c1377502ceb29df169cab61197
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-03-31 22:02:57 -0300

    [refact]: controladores validam: autoria , inputs e a existencia do resurso na base

--- bin/controllers/companies_security_controller.dart
+++ bin/controllers/companies_security_controller.dart
@@ -18,17 +18,18 @@ class CompaniesSecurityController extends Controller {
     Router router = Router();
 
     router.get('/companies', (Request request) async {
-      // verificação do created_by
-      final List<CompanyModel?> result = await _companiesService.findByQuery();
+      final userID = _getUserIdFromJWT(request);
+      final List<CompanyModel?> result = await _companiesService.findByQuery(
+          queryParam: "t1.created_by = '$userID' and t1.status = 'active';");
       return Response.ok(jsonEncode(result));
     });
 
     router.post('/companies', (Request request) async {
       final String body = await request.readAsString();
       final CompanyModel companyModel = CompanyModel.fromMap(jsonDecode(body));
-      JWT jwt = request.context['jwt'] as JWT;
-      final String userIdFromJWT = jwt.payload['userID'];
+      final String userIdFromJWT = _getUserIdFromJWT(request);
       companyModel.createdBy = userIdFromJWT;
+      companyModel.status = 'active';
       final result = await _companiesService.save(companyModel);
       return result ? Response(201) : Response(404);
     });
@@ -54,7 +55,10 @@ class CompaniesSecurityController extends Controller {
         return Response.forbidden('Not Authorized.');
       }
 
-      var result = await _companiesService.save(companyModel);
+      final userID = _getUserIdFromJWT(request);
+      companyModel.updatedBy = userID;
+
+      final bool result = await _companiesService.save(companyModel);
       return result ? Response(201) : Response(500);
     });
 
@@ -62,23 +66,41 @@ class CompaniesSecurityController extends Controller {
       final String body = await request.readAsString();
       final StatusTO statusTO = StatusTO.fromRequest(body);
       CompanyModel companyModel = CompanyModel();
-      companyModel.status = statusTO.status;
+
+      companyModel.status = statusTO.status.toLowerCase();
       companyModel.id = statusTO.id;
 
       if (companyModel.id == null || companyModel.status == null) {
         return Response.badRequest();
       }
 
-      switch (companyModel.status) {
-        case "active":
-        case "inactive":
-          break;
-        default:
-          return Response.badRequest();
+      final List<String> listOfStatus = await _companiesService.getStatus();
+
+      if (!listOfStatus.contains(companyModel.status?.toLowerCase())) {
+        return Response.badRequest();
       }
 
-      //validar se usuario possui autorização para pausar a vaga
+      final CompanyModel? companyModelFromDB =
+          await _companiesService.findOne(companyModel.id!);
 
+      if (companyModelFromDB == null) {
+        return Response.notFound('Not Found');
+      }
+      companyModel.createdBy = companyModelFromDB.createdBy;
+
+      final JWT jwt = request.context['jwt'] as JWT;
+      final String profileID = jwt.payload['roles'];
+      bool valid = true;
+      if (!(profileID == "Admin")) {
+        valid = await _validateAuth(companyModel, request);
+      }
+
+      if (!valid) {
+        return Response.forbidden('Not Authorized.');
+      }
+
+      final String userIdFromJWT = _getUserIdFromJWT(request);
+      companyModel.updatedBy = userIdFromJWT;
       bool result = await _companiesService.updateStatus(companyModel);
 
       return result ? Response(201) : Response(500);
@@ -90,13 +112,16 @@ class CompaniesSecurityController extends Controller {
 
   Future<bool> _validateAuth(CompanyModel companyModel, Request request) async {
     final String createdBy = companyModel.createdBy!;
-    JWT jwt = request.context['jwt'] as JWT;
-    final String userIdFromJWT = jwt.payload['userID'];
-
+    final String userIdFromJWT = _getUserIdFromJWT(request);
     if (userIdFromJWT != createdBy) {
       return false;
     }
-
     return true;
+  }
+
+  String _getUserIdFromJWT(Request request) {
+    final JWT jwt = request.context['jwt'] as JWT;
+    final userID = jwt.payload['userID'];
+    return userID;
   }
 }
