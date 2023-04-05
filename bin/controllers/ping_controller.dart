5d6f39df40ef6eedb253c27516e651ef10c7b030
Valdir Giorgi Notebook <valdirgiorgi@gmail.com>
2023-04-05 12:01:57 -0300

    atualizando Ping com informações do sistema

--- bin/controllers/ping_controller.dart
+++ bin/controllers/ping_controller.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:linux_system_info/linux_system_info.dart';
 import 'package:shelf/shelf.dart';
 import 'package:shelf_router/shelf_router.dart';
 
@@ -7,7 +10,20 @@ class PingController {
 
     router.get('/ping', (Request request) async {
       final now = DateTime.now();
-      return Response.ok(now.toString());
+      String result = '';
+
+      if (Platform.isLinux) {
+        //CPU
+        var cpuUsage = CpuInfo.getCpuUsagePercentage();
+        //MEMORY
+        var totalMem = MemInfo().mem_free_mb;
+
+        result = '$now CPU Usage:$cpuUsage Memory Free:$totalMem';
+      } else {
+        result = now.toString();
+      }
+
+      return Response.ok(result);
     });
 
     return router;
