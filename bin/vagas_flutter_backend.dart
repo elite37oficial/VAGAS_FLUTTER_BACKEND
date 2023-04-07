fc4388d764943dfb66fbe9271c5fc68a37bd65ac
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-04-07 14:57:43 -0300

    [refact]: instancia segurança globalmente e remove middlewares por controlador

--- bin/vagas_flutter_backend.dart
+++ bin/vagas_flutter_backend.dart
@@ -11,24 +11,29 @@ import 'core/custom_server.dart';
 import 'package:shelf/shelf.dart';
 import 'package:dotenv/dotenv.dart';
 
+import 'core/security/security_service.dart';
+
 void main() async {
   var env = DotEnv(includePlatformEnvironment: true)..load();
 
   final di = Injects.initialize();
-
   final cascade = Cascade()
       .add(di.get<LoginController>().getHandler())
       .add(di.get<PingController>().handler)
       .add(di.get<JobsController>().getHandler())
       .add(di.get<UsersController>().getHandler())
-      .add(di.get<UsersSecurityController>().getHandler(isSecurity: true))
-      .add(di.get<JobsSecurityController>().getHandler(isSecurity: true))
-      .add(di.get<CompaniesSecurityController>().getHandler(isSecurity: true))
+      .add(di.get<UsersSecurityController>().getHandler())
+      .add(di.get<JobsSecurityController>().getHandler())
+      .add(di.get<CompaniesSecurityController>().getHandler())
       .handler;
+
+  final SecurityService securityService = di.get<SecurityService>();
 
   final pipeline = Pipeline()
       .addMiddleware(logRequests())
       .addMiddleware(MiddlewareInterception().appJson)
+      .addMiddleware(securityService.authorization)
+      .addMiddleware(securityService.verifyJwt)
       .addHandler(cascade);
 
   await CustomServer().initilize(
