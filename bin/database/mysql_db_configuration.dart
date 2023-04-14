96ac86f0ff4c399d34aa21880065bcff66e786ed
Luan Fonseca Torralbo Gimenez <51548623+Luanftg@users.noreply.github.com>
2023-04-14 17:02:01 -0300

    [refact]: aumenta a o timeout das conexoes  com o banco

--- bin/database/mysql_db_configuration.dart
+++ bin/database/mysql_db_configuration.dart
@@ -17,11 +17,14 @@ class MySqlDbConfiguration implements DBConfiguration {
   @override
   Future<MySqlConnection> createConection() async {
     var configuration = ConnectionSettings(
-        db: env['db_schema'],
-        host: env['db_host'] ?? '',
-        port: int.parse(env['db_port'].toString()),
-        password: env['db_pass'],
-        user: env['db_user']);
+      db: env['db_schema'],
+      host: env['db_host'] ?? '',
+      port: int.parse(env['db_port'].toString()),
+      password: env['db_pass'],
+      user: env['db_user'],
+      timeout: Duration(seconds: 60),
+    );
+
     return await MySqlConnection.connect(configuration);
   }
 
