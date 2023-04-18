0d545c979d5c4c4a80b90c311869fdfa5c7055a7
Valdir Giorgi Notebook <valdirgiorgi@gmail.com>
2023-04-18 11:28:31 -0300

    atualizando a versão

--- bin/controllers/ping_controller.dart
+++ bin/controllers/ping_controller.dart
@@ -7,7 +7,7 @@ import 'package:shelf_router/shelf_router.dart';
 class PingController {
   Handler get handler {
     var router = Router();
-    const String buildNumber = '0.0.1';
+    const String buildNumber = '0.0.2';
 
     router.get('/ping', (Request request) async {
       final now = DateTime.now();
